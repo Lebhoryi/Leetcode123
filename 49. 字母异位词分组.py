8c7a764a212d58d446b99bf81ab975f59af525b8
Lebhroryi <Lebhoryi@gmail.com>
2020-02-17 00:27:42 +0800

    groupAnagrams

---
+++ 49. 字母异位词分组.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+'''
+@ Summary: 
+@ Update:  
+
+@ file:    49. 字母异位词分组.py
+@ version: 1.0.0
+
+@ Author:  Lebhoryi@gmail.com
+@ Date:    2/17/20 12:02 AM
+'''
+def groupAnagrams(strs: [str]) -> [[str]]:
+    d = {}
+    for s in strs:
+        k = "".join(sorted(s))
+        d[k] = d.get(k, []) + [s]
+    return list(d.values())
+
+
+d = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(groupAnagrams(d))
