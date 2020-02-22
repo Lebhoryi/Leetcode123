0d298613d5da7b3badcdb32efb1ca206a36b2486
Lebhroryi <Lebhoryi@gmail.com>
2020-02-22 23:35:28 +0800

    mySqrt

---
+++ 69. x 的平方根.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+'''
+@ Summary: 
+@ Update:  
+
+@ file:    69. x 的平方根.py
+@ version: 1.0.0
+
+@ Author:  Lebhoryi@gmail.com
+@ Date:    2/22/20 11:33 PM
+'''
+import math
+def mySqrt(x: int) -> int:
+    return int(x ** 0.5)
+
+x = 8
+print(mySqrt(x))
